6a572f8db25c39aeee3a086ea7237a050727045a
furkansade <20080069@stu.omu.edu.tr>
2022-11-15 15:19:10 +0300

    add-pq-package

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq" // auxiliary tool taht will work with database/sql
+)
 
 const (
 	host     = "localhost"
-	port     = "5432"
+	port     = 5432
 	user     = "postgres"
 	password = "postgres"
 	dbname   = "productsdb"
 )
 
 var db *sql.DB // database/sql package pointer's
+
+// initialization func
+func init() {
+	var err error
+	connString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname) // connection string
+
+	db, err = sql.Open("postgres", connString) // open the connection
+
+}
 
 func main() {
 
