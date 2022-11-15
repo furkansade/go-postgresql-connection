8f2010b109ff8c8d944d29ce74e41dac7997a0f3
furkansade <20080069@stu.omu.edu.tr>
2022-11-15 15:02:54 +0300

    start-go-postgreSql-connection

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "database/sql"
+
+const (
+	host     = "localhost"
+	port     = "5432"
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "productsdb"
+)
+
+var db *sql.DB // database/sql package pointer's
+
+func main() {
+
+}
